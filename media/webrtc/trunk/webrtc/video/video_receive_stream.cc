44a2b322658539586cee4ef7dca72c90bbb161a4
Sergio Mena <semena@cisco.com>
2019-03-19 13:39:36 +0100

    Removed ccfb-related printfs

--- media/webrtc/trunk/webrtc/video/video_receive_stream.cc
+++ media/webrtc/trunk/webrtc/video/video_receive_stream.cc
@@ -39,12 +39,10 @@ namespace webrtc {
 
 static CongestionController::SendSideBwe UseSendSideBwe(const VideoReceiveStream::Config& config) {
   if (config.rtp.ccfb) {
-    printf("\t\t\t\tVideo receive stream detected ffcb when creating stream receiver\n");
     for (const auto& extension : config.rtp.extensions) {
       if (extension.uri == RtpExtension::kCCFBFlagUri)
         return CongestionController::kBweCcfb;
     }
-    printf("\t\t\t\tVideo receive stream missing kCCFBFlagUri in extensions\n");
     return CongestionController::kBweNone;
   }
   if (!config.rtp.transport_cc)
--- media/webrtc/trunk/webrtc/voice_engine/channel.cc
+++ media/webrtc/trunk/webrtc/voice_engine/channel.cc
@@ -2641,7 +2641,6 @@ void Channel::EnableSendTransportSequenceNumber(int id) {
   int ret =
       SetSendRtpHeaderExtension(true, kRtpExtensionTransportSequenceNumber, id);
   //TODO Is this audio only?
-  printf("\t\t\t\tEnableSendTransportSequenceNumber should be audio-only\n");
   RTC_DCHECK_EQ(0, ret);
 }
 
@@ -2651,7 +2650,6 @@ void Channel::EnableReceiveTransportSequenceNumber(int id) {
   bool ret = rtp_header_parser_->RegisterRtpHeaderExtension(
       kRtpExtensionTransportSequenceNumber, id);
   //TODO Is this audio only?
-  printf("\t\t\t\tEnableReceiveTransportSequenceNumber should be audio-only\n");
   RTC_DCHECK(ret);
 }
 
